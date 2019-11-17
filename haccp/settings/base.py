f34722905adc5e2606610abc61d6480df3d30557
ldom <louis.giron@ensae-paristech.fr>
2019-11-17 21:30:28 +0100

    update production.py

--- haccp/settings/base.py
+++ haccp/settings/base.py
@@ -146,6 +146,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "live-static"),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
--- haccp/settings/production.py
+++ haccp/settings/production.py
@@ -80,6 +80,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "live-static", "static-root"),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
