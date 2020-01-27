54cbd3574657340cd39327358a2516f1d237a168
ldom <louis.giron@ensae-paristech.fr>
2020-01-28 00:07:30 +0100

    resolve migration

--- apps/plan_nettoyage/forms.py
+++ apps/plan_nettoyage/forms.py
@@ -12,21 +12,22 @@ class PlanNettoyageForm(ModelForm):
 
     piece = apps.get_model('piece', 'Piece').objects.all()
     collaborateurs = apps.get_model('collaborateurs', 'Collaborateur').objects.all()
-    '''piece = apps.get_model('piece', 'Piece').objects.all()
+    #piece = apps.get_model('piece', 'Piece').objects.all()
 
     choices_first_name_collaborateur = [[1, 'Choose...']] + [[y.first_name, y.first_name] for y in collaborateurs]
     choices_last_name_collaborateur = [[1, 'Choose...']] + [[y.last_name, y.last_name] for y in collaborateurs]
-    choices_piece = [[1, 'Choose...']] + [[y.name_piece, y.name_piece] for y in piece]
+    #choices_piece = [[1, 'Choose...']] + [[y.name_piece, y.name_piece] for y in piece]
     
     widgets = {
       'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
       'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
       'first_name_collaborateur': forms.Select(choices=choices_first_name_collaborateur,  attrs={'class': 'form-control'}),
       'last_name_collaborateur': forms.Select(choices=choices_last_name_collaborateur, attrs={'class': 'form-control'}),
-      'piece': forms.Select(choices=choices_piece, attrs={'class': 'form-control'}),
+      #'piece': forms.Select(choices=choices_piece, attrs={'class': 'form-control'}),
     }
 
-    fields = ('title', 'start_time', 'end_time', 'description', 'first_name_collaborateur', 'last_name_collaborateur', 'piece')
+    #fields = ('title', 'start_time', 'end_time', 'description', 'first_name_collaborateur', 'last_name_collaborateur', 'piece')
+    fields = ('title', 'start_time', 'end_time', 'description', 'first_name_collaborateur', 'last_name_collaborateur')
 
   def __init__(self, *args, **kwargs):
 
@@ -34,4 +35,4 @@ class PlanNettoyageForm(ModelForm):
     
     # input_formats parses HTML5 datetime-local input to datetime field
     self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
-    self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)'''
+    self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
